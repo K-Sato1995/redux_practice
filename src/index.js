cb4f0fd724626340e77c58072a495aeb3d18ec1f
Katsuki <Katsuki_Sato0130@icloud.com>
2018-11-10 16:57:02 +0900

    Modified index also ran eslint --fix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 import { createStore } from 'redux';
 import tasksReducer from './reducers/tasks';
+import addTask from './actions/tasks';
+
 // ReducerとcreateStoreを用いてstoreの作成
 const store = createStore(tasksReducer);
 
 // 1つのReduxアプリに関して1つのstoreだけが存在する。
 // 様々なデータを扱う際にはreducerを細分化する事で対応する。
+
+// storeは以下の４つのメソッドを持つ。
+
+// dispatch: アクションを発行（実行）。
+// subscribe: stateの状態が変化した際のコールバック関数の設定。
+// getState: 現在のstateの状態を取得。
+
+console.log(store.getState()); //=> {tasks: []} initialState
+store.dispatch(addTask('New Task'))
