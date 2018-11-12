f8d8a982f306d97e0c1cc12bd62b28bf153c1448
Katsuki <Katsuki_Sato0130@icloud.com>
2018-11-12 20:25:56 +0900

    added function to iterate tasks

--- src/components/TodoApp.jsx
+++ src/components/TodoApp.jsx
@@ -3,20 +3,19 @@ import React from 'react';
 // Containerから整形されたオブジェクトがPropsとして渡ってくる。
 class TodoApp extends React.Component {
   render() {
-    const list =
-      this.props.tasks.map = (task) => {
-        <li>
-          { task }
-        </li>
-      }
-
     return (
       <div>
         {/* containerから渡されたものをthis.propsで取得可能*/}
-        <input type = 'text' onChange={(e) => this.props.inputTask(e.target.value)} />
-        <input value = 'Add' type = 'button' onClick={() => this.props.addTask(this.props.task)} />
+        <input type = 'text' onChange = { (e) =>  this.props.inputTask(e.target.value) }/>
+        <button onClick = { (e) => this.props.addTask(this.props.task) }>Click</button>
         <ul>
-          { list }
+          {
+            this.props.tasks.map(function(item, i) {
+              return (
+                <li key = {i}>{item}</li>
+              )
+            })
+          }
         </ul>
       </div>
     )
